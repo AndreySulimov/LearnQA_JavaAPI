f6f1c92baa8ac07991a7c57e259c96530166aa75
AndreySulimov <sulimov.andrei@bk.ru>
2024-02-27 15:32:16 +0300

    HelloWorldTest updates

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,14 +1,30 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import io.restassured.RestAssured;
+import io.restassured.http.Headers;
 import io.restassured.response.Response;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
 
     @Test
-    public void testHelloWorld() {
+    public void testRestAssured() {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("myHeader1", "value1");
+        headers.put("myHeader2", "value2");
+
         Response response = RestAssured
-            .get("https://playground.learnqa.ru/api/hello")
+            .given()
+            .get("https://playground.learnqa.ru/api/map2")
             .andReturn();
+
+        assertEquals(200, response.statusCode(), "Unexpected status code");
         response.prettyPrint();
+
+        Headers responseHeaders = response.getHeaders();
+        System.out.println(responseHeaders);
     }
 }
